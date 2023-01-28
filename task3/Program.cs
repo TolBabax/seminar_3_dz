e672da5eb76244ffcb76c81b0a2c396a6840be99
TolBabax <kuz.cerg@gmail.com>
2023-01-28 19:38:17 +0400

    программа, которая выдает таблицу кубов от 1 до N

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 0)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        System.Console.WriteLine(Math.Pow(i, 3));
+    }
+}
+else
+{
+    System.Console.WriteLine("Введите число N больше 0");
+}
